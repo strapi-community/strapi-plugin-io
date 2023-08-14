a0b4085de7ef20378950341ab9f6c163170e1e7b
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2023-08-13 23:51:39 -0400

    refactor(config): improve schema

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 'use strict';
 
 module.exports = {
-	$schema: 'https://json.schemastore.org/eslintrc',
-	extends: ['@strapi/eslint-config/back'],
-	parserOptions: {
-		ecmaVersion: 2020,
-	},
-	globals: {
-		strapi: false,
-	},
-	rules: {
-		'import/no-dynamic-require': 'off',
-		'global-require': 'off',
-		'prefer-destructuring': ['error', { AssignmentExpression: { array: false } }],
-		'no-underscore-dangle': 'off',
-		'no-use-before-define': 'off',
-		'no-continue': 'warn',
-		'no-process-exit': 'off',
-		'no-loop-func': 'off',
-		'max-classes-per-file': 'off',
-		'no-param-reassign': [
-			'error',
-			{
-				props: false,
-			},
-		],
-	},
+  $schema: 'https://json.schemastore.org/eslintrc',
+  extends: ['@strapi/eslint-config/back'],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  globals: {
+    strapi: false,
+  },
+  rules: {
+    'import/no-dynamic-require': 'off',
+    'global-require': 'off',
+    'prefer-destructuring': ['error', { AssignmentExpression: { array: false } }],
+    'no-underscore-dangle': 'off',
+    'no-use-before-define': 'off',
+    'no-continue': 'warn',
+    'no-process-exit': 'off',
+    'no-loop-func': 'off',
+    'max-classes-per-file': 'off',
+    'no-param-reassign': [
+      'error',
+      {
+        props: false,
+      },
+    ],
+  },
 };
