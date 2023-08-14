bd4fa2530aba48351d31b118b09576aaa7d69979
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2023-08-13 23:44:11 -0400

    chore(prettier): remove redundant rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,5 @@
 module.exports = {
-	$schema: 'http://json.schemastore.org/prettierrc',
-	endOfLine: 'lf',
-	semi: true,
-	singleQuote: true,
-	tabWidth: 2,
-	trailingComma: 'es5',
-	printWidth: 100,
-	arrowParens: 'always',
+  $schema: 'http://json.schemastore.org/prettierrc',
+  useTabs: true,
+  printWidth: 120,
 };
