a33efdb7b3565ebf5d8c81b37d541bb637620b22
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2023-08-13 23:51:39 -0400

    refactor(config): improve schema

--- server/config/schema.js
+++ server/config/schema.js
@@ -2,18 +2,37 @@
 
 const yup = require('yup');
 
+const contentTypeConfig = yup.lazy((value) => {
+	if (typeof value === 'string') {
+		return yup.string();
+	}
+
+	if (typeof value === 'object') {
+		return yup.object().shape({
+			uid: yup.string(),
+			actions: yup.array().oneOf(['create', 'update', 'delete']).optional(),
+		});
+	}
+
+	return false;
+});
+
 const pluginConfigSchema = yup.object().shape({
-	events: yup.object(),
+	events: yup.object().optional(),
 	hooks: yup.object().shape({
-		init: yup.object().test({
-			name: 'init',
-			exclusive: true,
-			// eslint-disable-next-line no-template-curly-in-string
-			message: '${path} must be a function',
-			test: (value) => typeof value === 'function',
-		}),
-	}),
-	socket: yup.object().shape({ serverOptions: yup.object() }),
+		init: yup
+			.object()
+			.test({
+				name: 'init',
+				exclusive: true,
+				// eslint-disable-next-line no-template-curly-in-string
+				message: '${path} must be a function',
+				test: (value) => typeof value === 'function',
+			})
+			.optional(),
+	}).optional(),
+	contentTypes: yup.array().of(contentTypeConfig),
+	socket: yup.object().shape({ serverOptions: yup.object().optional() }).optional(),
 });
 
 module.exports = {
