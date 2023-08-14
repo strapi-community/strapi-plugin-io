9c7c677e401a2f7262c05c78836ebeabf488061b
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2023-08-13 23:52:23 -0400

    refactor(lifecycles): use subscriber map and register each per model

--- server/bootstrap/lifecycle.js
+++ server/bootstrap/lifecycle.js
@@ -1,52 +1,5 @@
 'use strict';
 
-const ALLOWED_ACTIONS = new Set([
-	'afterCreate',
-	'afterCreateMany',
-	'afterUpdate',
-	'afterUpdateMany',
-	'afterDelete',
-	'afterDeleteMany',
-	'afterFindOne',
-	'afterFindMany',
-]);
-
-function isAllowedAction(action) {
-	return action && ALLOWED_ACTIONS.has(action);
-}
-
-function isAPIModel(model) {
-	return model && model.uid && /^api::\w+/i.test(model.uid);
-}
-
-function getEventType(action) {
-	let event = '';
-	switch (action) {
-		case 'afterCreate':
-		case 'afterCreateMany':
-			event = 'create';
-			break;
-		case 'afterFindOne':
-			event = 'findOne';
-			break;
-		case 'afterFindMany':
-			event = 'find';
-			break;
-		case 'afterUpdate':
-		case 'afterUpdateMany':
-			event = 'update';
-			break;
-		case 'afterDelete':
-		case 'afterDeleteMany':
-			event = 'delete';
-			break;
-		default:
-			event = '';
-			break;
-	}
-	return event;
-}
-
 /**
  * Bootstrap lifecycles
  *
@@ -54,16 +7,133 @@ function getEventType(action) {
  * @param {*} params.strapi
  */
 async function bootstrapLifecycles({ strapi }) {
-	// setup lifecycles
-	strapi.db.lifecycles.subscribe((event) => {
-		if (isAllowedAction(event.action) && isAPIModel(event.model)) {
-			strapi.$io.emit({
-				event: getEventType(event.action),
-				schema: event.model,
-				data: event.result,
-			});
+	strapi.config.get('plugin.io.contentTypes', []).forEach((ct) => {
+		const uid = ct.uid ? ct.uid : ct;
+
+		const subscriber = {
+			models: [uid],
+		};
+
+		if (!ct.actions || ct.actions.includes('create')) {
+			const eventType = 'create';
+			subscriber.afterCreate = async (event) => {
+				strapi.$io.emit({
+					event: eventType,
+					schema: event.model,
+					data: event.result,
+				});
+			};
+			subscriber.afterCreateMany = async (event) => {
+				const query = buildEventQuery({ event });
+				if (query.filters) {
+					const records = await strapi.entityService.findMany(uid, query);
+
+					records.forEach((r) => {
+						strapi.$io.emit({
+							event: eventType,
+							schema: event.model,
+							data: r,
+						});
+					});
+				}
+			};
 		}
+
+		if (!ct.actions || ct.actions.includes('update')) {
+			const eventType = 'update';
+			subscriber.afterUpdate = async (event) => {
+				strapi.$io.emit({
+					event: eventType,
+					schema: event.model,
+					data: event.result,
+				});
+			};
+			subscriber.beforeUpdateMany = async (event) => {
+				const query = buildEventQuery({ event });
+				if (query.filters) {
+					const ids = await strapi.entityService.findMany(uid, query);
+					if (!event.state.io) {
+						event.state.io = {};
+					}
+					event.state.io.ids = ids;
+				}
+			};
+			subscriber.afterUpdateMany = async (event) => {
+				if (!event.state.io?.ids) {
+					return;
+				}
+				const records = await strapi.entityService.findMany(uid, {
+					filters: { id: event.state.io.ids },
+				});
+
+				records.forEach((r) => {
+					strapi.$io.emit({
+						event: eventType,
+						schema: event.model,
+						data: r,
+					});
+				});
+			};
+		}
+
+		if (!ct.actions || ct.actions.includes('delete')) {
+			const eventType = 'delete';
+			subscriber.afterDelete = async (event) => {
+				strapi.$io.emit({
+					event: eventType,
+					schema: event.model,
+					data: event.result,
+				});
+			};
+			subscriber.beforeDeleteMany = async (event) => {
+				const query = buildEventQuery({ event });
+				if (query.filters) {
+					const records = await strapi.entityService.findMany(uid, query);
+					if (!event.state.io) {
+						event.state.io = {};
+					}
+					event.state.io.records = records;
+				}
+			};
+			subscriber.afterDeleteMany = async (event) => {
+				if (!event.state.io?.records) {
+					return;
+				}
+				event.state.io.records.forEach((r) => {
+					strapi.$io.emit({
+						event: eventType,
+						schema: event.model,
+						data: r,
+					});
+				});
+			};
+		}
+
+		// setup lifecycles
+		strapi.db.lifecycles.subscribe(subscriber);
 	});
+}
+
+function buildEventQuery({ event }) {
+	const query = {};
+
+	if (event.params.where) {
+		query.filters = event.params.where;
+	}
+
+	if (event.result?.count) {
+		query.limit = event.result.count;
+	} else if (event.params.limit) {
+		query.limit = event.params.limit;
+	}
+
+	if (event.action === 'afterCreateMany') {
+		query.filters = { id: event.result.ids };
+	} else if (event.action === 'beforeUpdate') {
+		query.fields = ['id'];
+	}
+
+	return query;
 }
 
 module.exports = { bootstrapLifecycles };
