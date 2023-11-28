91f372a993ae3882372e21795ef4778403363d1c
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2023-11-28 07:20:41 -0500

    fix(config): invalid events type default

--- server/config/index.js
+++ server/config/index.js
@@ -5,7 +5,7 @@ const { plugin } = require('./schema');
 module.exports = {
 	default() {
 		return {
-			events: {},
+			events: [],
 			hooks: {},
 			socket: { serverOptions: { cors: { origin: 'http://127.0.0.1:8080', methods: ['GET', 'POST'] } } },
 		};
